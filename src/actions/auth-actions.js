cb3901d6b145a7a1b81fe20326695491e203fd43
osledrobilka <osledrobilka@gmail.com>
2017-05-13 00:18:50 -0500

    creates auth-actions.js & adds content

---
+++ src/actions/auth-actions.js
@@ -0,0 +1,140 @@
+import { Actions } from 'react-native-router-flux';
+import { AsyncStorage } from 'react-native';
+import firebase from 'firebase';
+import jwtDecode from 'jwt-decode';
+
+import {
+    EMAIL_CHANGED,
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL,
+    LOGIN_USER_START,
+    LOGIN_ERROR,
+    LOGOUT_USER_SUCCESS,
+    LOGOUT_USER_FAIL
+} from './types';
+
+export const emailChanged = (text) => ({
+    type: EMAIL_CHANGED,
+    data: text
+});
+
+export const passwordChanged = (text) => ({
+    type: PASSWORD_CHANGED,
+    data: text
+});
+
+export const loginError = (error) => ({
+    type: LOGIN_ERROR,
+    data: error
+});
+
+export const loginUserStart = () => ({
+    type: LOGIN_USER_START
+});
+
+export const loginUser = ({ emailAddress, password }) => {
+     return (dispatch) => {
+         firebase.auth().signInWithEmailAndPassword(emailAddress, password)
+             .then(user => {
+                AsyncStorage.mergeItem('authData', JSON.stringify(user))
+                .then(() => {
+                    const name = user.providerData[0].displayName || '';
+                    const email = user.providerData[0].email || '';
+                    const profileImageUrl = user.providerData[0].photoURL || '';
+                    return loginUserSuccess(dispatch, user, {
+                        name,
+                        email,
+                        profileImageUrl
+                    });
+                });
+                const name = user.providerData[0].displayName || '';
+                const email = user.providerData[0].email || '';
+                const profileImageUrl = user.providerData[0].photoURL || '';
+                return loginUserSuccess(dispatch, user, {
+                    name,
+                    email,
+                    profileImageUrl
+                });
+             })
+             .catch(() => {
+                 loginUserFail(dispatch, 'Invalid email/password combination.');
+             });
+     };
+ };
+
+ export const registerUser = ({ emailAddress, password }) => {
+    return (dispatch) => {
+        firebase.auth().createUserWithEmailAndPassword(emailAddress, password)
+            .then(authData => {
+                const name = authData.providerData[0].displayName || '';
+                const email = authData.providerData[0].email || '';
+                const profileImageUrl = authData.providerData[0].photoURL || '';
+                return loginUserSuccess(dispatch, authData, {
+                    name,
+                    email,
+                    profileImageUrl
+                });
+            })
+            .catch((error) => {
+                const code = error.code;
+                if (code === 'auth/weak-password') {
+                    loginUserFail(dispatch, 'A stronger password is required.');
+                } else if (code === 'auth/email-already-in-use') {
+                    loginUserFail(
+                        dispatch,
+                        'Email is already registered. Please log in.'
+                    );
+                } else {
+                    loginUserFail(dispatch, 'Registration Failed.');
+                }
+                console.log(error);
+            });
+    };
+};
+
+export const logoutUser = () => {
+    return (dispatch) => {
+        firebase.auth().signOut()
+            .then(() => {
+                logoutUserSuccess(dispatch, 'You were successfully logged out.');
+            })
+            .catch(() => {
+                logoutUserFail(dispatch, 'Logout unsuccessful, please try again.');
+            });
+    };
+};
+
+const loginUserSuccess = (dispatch, user, authData) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        data: {
+            user,
+            authData
+        }
+    });
+    Actions.bluetooth();
+};
+
+const loginUserFail = (dispatch, error) => {
+    dispatch({
+        type: LOGIN_USER_FAIL,
+        data: error
+    });
+};
+
+const logoutUserSuccess = (dispatch, message) => {
+    dispatch({
+        type: LOGOUT_USER_SUCCESS,
+        data: message
+    });
+
+    Actions.auth();
+};
+
+const logoutUserFail = (dispatch, error) => {
+    dispatch({
+        type: LOGOUT_USER_FAIL,
+        data: error
+    });
+};
